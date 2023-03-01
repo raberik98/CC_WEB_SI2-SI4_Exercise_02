8e3fb0bdab34fb97885d22e9cbd9db94f206c2d9
Erik <erik.raboczki98@gmail.com>
2023-03-01 08:57:11 +0100

    1.0 starting files added

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # CC_WEB_SI2-SI4_Exercise_02
 Task name: Company Budget and Employees handler 
+
+
+You are hired to create a project where the finance department can handle employees and the company budget.
+The starting files are already added as well as some starting data.
+
+1; Set up a backend server which will be able to handle our database.
+
+2; Create an endpoint where we can get all the data from a specific employee but not the CEO, when someone attempts to request data from the CEO respond with an access denied error message.
+
+3; Make our application be able to register new employees, and to also fire employees.
+
+4; Create a middleware which will write the current company budget to the console every time we are triggering one of our endpoints.
+
+5; Make our application be able to give a raise to an employee. (Set the employee's salary to a given amount)
+
+TO BE CONTINUED...
+
---
+++ index.js
@@ -0,0 +1,48 @@
+let data = {
+    companyBudget: 1000000,
+    employees: [
+        {
+            id:1,
+            name: "Bossy Bob",
+            salary: 750000,
+            role: "CEO"
+        },
+        {
+            id:2,
+            name: "Toxic Tiffany",
+            salary: 350000,
+            role: "HR"
+        },
+        {
+            id:3,
+            name: "Trusty Tom",
+            salary: 500000,
+            role: "Team lead"
+        },
+        {
+            id:4,
+            name: "Hardworking Harley",
+            salary: 500000,
+            role: "Software developer"
+        },
+        {
+            id:5,
+            name: "Test Elek",
+            salary: 450000,
+            role: "Software tester"
+        },
+        {
+            id:6,
+            name: "Alcoholic Andrew",
+            salary: 200000,
+            role: "Maintenance"
+        },
+    ],
+    transactionHistory: [
+        {
+            id:0001,
+            amount: 0,
+            to:1
+        }
+    ]
+}
